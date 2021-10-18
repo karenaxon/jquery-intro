07a13281f9341e0f0eb740fcdfe6a84b1a704f58
Kaxon <karenaxon@users.noreply.github.com>
2021-10-18 15:03:47 -0700

    initial files.
    Co-authored-by: Aaron Ewert <aaronjewert@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+jQuery("h1").click(function() {
+  alert("This is a heading.");
+  alert("I told you, THIS IS A HEADING!");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+// jQuery("img").click(function() {
+//   alert("This is an image.");
+// });
